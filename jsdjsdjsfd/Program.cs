2d38658ccfdfc3e35b7f5982bc4a8365dabf7f78
Ludwig Langell <ludwig.langell@elev.ga.ntig.se>
2023-10-04 10:16:38 +0200

    luck

---
+++ jsdjsdjsfd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+string Spel = "";
+string FavMat = "";
+string Skola = "";
+
+Console.WriteLine("Vilket spel är din favorit");
+
+Spel = Console.ReadLine();
+
+Console.WriteLine("Välj en mat du gillar");
+
+FavMat = Console.ReadLine();
+
+
+Console.WriteLine("Vilken skola går du på");
+
+Skola = Console.ReadLine();
+
+Console.WriteLine($"nämen wow jag spelar också {Spel} och du gillar {FavMat} och går på {Skola}." );
+Console.WriteLine("vi är ju typ nästan samma person");
+
+Console.WriteLine("nu måste jag dra dock, Ha en bra dag");
+Console.ReadLine();
+
